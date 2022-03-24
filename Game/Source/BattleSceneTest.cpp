bc71c04b148378d8a1f80e08c06537289f92246a
Victor <72089246+ZaroDev@users.noreply.github.com>
2022-03-24 12:44:42 +0100

    Update BattleSceneTest.cpp

--- Game/Source/BattleSceneTest.cpp
+++ Game/Source/BattleSceneTest.cpp
@@ -11,35 +11,35 @@ BattleSceneTest::~BattleSceneTest()
 bool BattleSceneTest::Awake(pugi::xml_node&)
 {
 	bool ret = true;
-	return true;
+	return ret;
 }
 
 bool BattleSceneTest::Start()
 {
 	bool ret = true;
-	return true;
+	return ret;
 }
 
 bool BattleSceneTest::PreUpdate()
 {
 	bool ret = true;
-	return true;
+	return ret;
 }
 
 bool BattleSceneTest::Update(float dt)
 {
 	bool ret = true;
-	return true;
+	return ret;
 }
 
 bool BattleSceneTest::PostUpdate()
 {
 	bool ret = true;
-	return true;
+	return ret;
 }
 
 bool BattleSceneTest::CleanUp()
 {
 	bool ret = true;
-	return true;
+	return ret;
 }
