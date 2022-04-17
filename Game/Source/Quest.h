8007be32b9e80153e3e0e48b5794ff741366fcf2
Lau <lauraisi2000@gmail.com>
2022-04-17 23:43:19 +0200

    quest manager
    not finished yet

---
+++ Game/Source/Quest.h
@@ -0,0 +1,58 @@
+#pragma once
+#include"SString.h"
+#include "PugiXml/src/pugixml.hpp"
+#include "SDL/include/SDL.h"
+
+
+enum QuestProgress { AVAILABLE, NOT_AVAILABLE, ACTIVE, COMPLETE, REWARDED }; //States a quest can have
+
+
+class Quest
+{
+
+public:
+
+
+	Quest(int id, SString title, SString description, int objective)
+	{
+		this->id = id;
+		progress = NOT_AVAILABLE;
+		this->title = title;
+		this->description = description;
+		this->objective = objective;
+	}
+
+	~Quest()
+	{
+
+	}
+
+public:
+
+
+
+	int id;							//Quest identification
+	QuestProgress progress;			//State of the current quest 
+	int nextQuest;					//next quest, if there is any (chain quest)
+
+	int objective;					//Posible quest objectives
+	bool reward=false;						//Posible quest rewards
+	int experience;
+
+	//int amount;
+
+	SString title;					//Title for the quest
+	SString description;			//What player has to know about the quest
+
+	//SDL_Texture* titleTex;
+	//SDL_Texture* descriptionTex;
+
+	//SDL_Rect rTitle, rDescription;
+
+	//virtual void Update();
+	//// Load / Save
+	//virtual bool LoadState(pugi::xml_node&);
+	//virtual bool SaveState(pugi::xml_node&) const;
+
+};
+
